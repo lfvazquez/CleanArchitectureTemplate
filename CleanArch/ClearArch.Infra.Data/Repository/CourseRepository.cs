aeb58947d25323f98beaa9e5a90573c50fba2234
LuisVazquez <lfvazquez@hotmail.com>
2021-09-13 21:23:51 -0500

    Commit implementation of Service and Repository GetCourses.

---
+++ CleanArch/ClearArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using ClearArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClearArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+
+    {
+        private UniversityDBContext _ctx;
+
+        public CourseRepository(UniversityDBContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}
