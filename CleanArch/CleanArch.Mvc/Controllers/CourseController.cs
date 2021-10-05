b641636a2d434f2764e44eb17a5041bb13a1a7e7
LuisVazquez <lfvazquez@hotmail.com>
2021-10-04 19:26:06 -0500

    Updating presentation layer

---
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _coursesService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _coursesService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            CourseViewModel model = _coursesService.GetCourses();
+
+            return View(model);
+        }
+    }
+}
